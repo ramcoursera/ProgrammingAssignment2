4dc3b70993e4107ab087a59b7b2301d9dc25fa8a
023423 <ramcoursera329@gmail.com>
2022-07-25 10:53:58 -0500

    week3

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,24 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# usage: 
+# mat1 <- matrix(c(5,1,0,3,-1,2,4,0,-1), nrow=3, ncol=3, byrow=TRUE)
+# cm <- makeCacheMatrix(mat1)
+# cacheSolve(cm)
+# cacheSolve(cm)
 
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inv) m <<- inv
+        getInv <- function() m
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
@@ -12,4 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInv(m)
+        m
 }
